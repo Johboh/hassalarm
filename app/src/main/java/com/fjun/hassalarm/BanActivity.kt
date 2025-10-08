18eef569cb28ce2175720f44e91cc6a95d7de150
Johan Böhlin <johboh@gmail.com>
2025-10-08 17:07:00 +0200

    Add input text for manually banning packages

--- app/src/main/java/com/fjun/hassalarm/BanActivity.kt
+++ app/src/main/java/com/fjun/hassalarm/BanActivity.kt
@@ -1,17 +1,22 @@
 package com.fjun.hassalarm
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fjun.hassalarm.databinding.ActivityBanlistBinding
+import java.util.regex.Pattern
 
 class BanActivity : AppCompatActivity() {
 
     private lateinit var adapter: BanAdapter
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +35,7 @@ class BanActivity : AppCompatActivity() {
                 missingPackageName
             }
 
-        val sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
         adapter = BanAdapter { packageName: String ->
             // REMOVE
             val set = sharedPreferences
@@ -38,8 +43,8 @@ class BanActivity : AppCompatActivity() {
             val newSet = set.toMutableSet().apply {
                 remove(packageName)
             }
-            save(sharedPreferences, newSet)
-            setList(sharedPreferences, adapter)
+            save(newSet)
+            setList()
         }
 
         with(binding) {
@@ -48,31 +53,64 @@ class BanActivity : AppCompatActivity() {
             list.layoutManager = LinearLayoutManager(this@BanActivity)
             list.adapter = adapter
             add.setOnClickListener {
-                // ADD
-                val creatorPackage = pendingIntent?.creatorPackage
-                if (!creatorPackage.isNullOrEmpty()) {
-                    val set = sharedPreferences
-                        .getStringSet(KEY_IGNORED_PACKAGES, HashSet<String>()) ?: setOf()
-                    val newSet = set.toMutableSet().apply {
-                        add(creatorPackage)
-                    }
-                    save(sharedPreferences, newSet)
-                    setList(sharedPreferences, adapter)
+                addNextAlarmPackage(pendingIntent)
+            }
+            addManual.isEnabled = false
+            manualPackageEditText.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    addManual.isEnabled = isValidPackageName(s.toString())
                 }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            })
+            addManual.setOnClickListener {
+                val manualPackage = manualPackageEditText.text.toString()
+                addPackage(manualPackage)
+                manualPackageEditText.text = null
             }
         }
 
-        setList(sharedPreferences, adapter)
+        setList()
     }
 
-    private fun setList(sharedPreferences: SharedPreferences, adapter: BanAdapter) {
+    private fun isValidPackageName(packageName: String) =
+        Pattern.matches("^[a-zA-Z_][a-zA-Z0-9_]*(\\.[a-zA-Z_][a-zA-Z0-9_]*)+$", packageName)
+
+    private fun addNextAlarmPackage(pendingIntent: PendingIntent?) {
+        val creatorPackage = pendingIntent?.creatorPackage
+        if (!creatorPackage.isNullOrEmpty()) {
+            addPackage(creatorPackage)
+        }
+    }
+
+    private fun addPackage(packageName: String) {
+        if (packageName.isNotEmpty()) {
+            val set = sharedPreferences
+                .getStringSet(KEY_IGNORED_PACKAGES, HashSet<String>()) ?: setOf()
+            val newSet = set.toMutableSet().apply {
+                add(packageName)
+            }
+            save(newSet)
+            setList()
+        }
+    }
+
+    private fun setList() {
         adapter.set(
             (sharedPreferences.getStringSet(KEY_IGNORED_PACKAGES, HashSet<String>())
                 ?: setOf()).toList()
         )
     }
 
-    private fun save(sharedPreferences: SharedPreferences, set: Set<String>) {
+    private fun save(set: Set<String>) {
         sharedPreferences.edit().putStringSet(KEY_IGNORED_PACKAGES, set)
             .apply()
     }
